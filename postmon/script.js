b7a4b82f5079438814c2d6c42a33e5753d3f7d01
Almir 'm3nd3s <m3nd3s@gmail.com>
2013-06-05 20:16:58 -0300

    Extensions básica para consulta a API

---
+++ postmon/script.js
@@ -0,0 +1,25 @@
+var postmon = {
+  query: function(){
+    var $cep = $("#cep");
+    var $endereco_tag = $("#endereco");
+
+    console.log($cep.val());
+    $.ajax({
+      url: "http://api.postmon.com.br/v1/cep/" + $cep.val(),
+      type: "GET",
+      cache: false,
+      dataType: "json"
+    })
+    .done(function(data){
+      var html = "Log: " + data.logradouro + "<br />";
+      html += "Bairro: " + data.bairro + "<br />";
+      html += "Cidade: " + data.cidade + "<br />";
+      $endereco_tag.html(html);
+    });
+  }
+}
+
+$("#consultar").click(function(e){
+  e.preventDefault();
+  postmon.query();
+});
